8c3be1229bb3a3e3ae5931e1e1c9d5965f0f0509
Nicolás López Flores <nikolaslf007@gmail.com>
2023-12-01 03:20:37 +0100

    Creacion del proyecto + prueba GetMapping

---
+++ src/main/java/com/collabpro/CollabProApplication.java
@@ -0,0 +1,16 @@
+package com.collabpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CollabProApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CollabProApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/collabpro/CollabProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collabpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
